e15216438f1a4e3e6052284dd69ec0a4de31d1fc
RT <rustytrim11@gmail.com>
2024-01-01 23:45:21 -0500

    Interface for user

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export interface ClientOptions {
+    
+}
+
+export interface UserPresence {
+    online: boolean;
+    status: string;
+    status_text: string;
+}
+
+export interface IUser {
+    id: string;
+    accent_color: number | null;
+    avatar: string | null;
+    avatar_decoration: string | null;
+    banned: boolean;
+    banner: string | null;
+    bot: boolean;
+    created_at: number;
+    discriminator: number;
+    edited_at: number;
+    flags: number;
+    global_name: string;
+    premium_type: number;
+    presence: UserPresence;
+    public_flags: number;
+    system: boolean;
+    username: string;
+    verified: boolean;
+}
