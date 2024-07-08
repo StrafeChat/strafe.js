6ad1dc5c5ece0cf3fe17d82ce08319be935a8d9b
ShadowLp174 <samuel.moravec@yahoo.com>
2024-07-08 12:58:17 +0200

    npm fixes

--- postinstall.js
+++ postinstall.js
@@ -3,8 +3,8 @@
 const path = require("path");
 const fs = require("fs");
 
-const workerPath = (path.resolve(__dirname, "../dist/worker.js")).replaceAll("\\", "\\\\");
-const indexPath = path.resolve(__dirname, "../dist/index.js");
+const workerPath = (path.resolve(__dirname, "./dist/worker.js")).replaceAll("\\", "\\\\");
+const indexPath = path.resolve(__dirname, "./dist/index.js");
 
 const data = fs.readFileSync(indexPath, "utf8");
 const result = data + "\nexports.workerPath = '" + workerPath + "'";
